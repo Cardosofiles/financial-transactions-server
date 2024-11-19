e9337219f2fef5afb9b73aac3573a6b61c0d9c44
cardosofiles <cardosofiles@outlook.com>
2024-11-18 22:31:02 -0300

    performing queries with knex

--- src/server.ts
+++ src/server.ts
@@ -1,11 +1,16 @@
 import fastify from "fastify";
+
+// module of creating id for registration
 import { knex } from "./database";
 
 const app = fastify();
 
 app.get("/hello", async (req, res) => {
-  const tables = await knex("sqlite_schema").select("*");
-  return tables;
+  const transactions = await knex("transactions")
+    .where("amount", 100)
+    .select("*");
+
+  return transactions;
 });
 
 app
@@ -15,3 +20,10 @@ app
   .then(() => {
     console.log("HTTP Server is running on port 3333");
   });
+
+// .insert({
+//   id: crypto.randomUUID(),
+//   title: "Test Transaction",
+//   amount: 100.0,
+// })
+// .returning("*");
