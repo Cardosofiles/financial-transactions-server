bce25cb1a7d23a02fae3cbb0f8b028baf27a06f2
cardosofiles <cardosofiles@outlook.com>
2024-11-20 12:02:10 -0300

    creating the get, post application routes

--- src/routes/transactions.ts
+++ src/routes/transactions.ts
@@ -5,6 +5,34 @@ import { randomUUID } from "node:crypto";
 import { knex } from "../database";
 
 export async function transactionsRoutes(app: FastifyInstance) {
+  app.get("/", async () => {
+    const transactions = await knex("transactions").select();
+    return {
+      transactions,
+    };
+  });
+
+  app.get("/:id", async (request) => {
+    const getTransactionParamsSchema = z.object({
+      id: z.string().uuid(),
+      // type: z.enum(["credit", "debit"]),
+    });
+
+    const { id } = getTransactionParamsSchema.parse(request.params);
+    const transaction = await knex("transactions").where("id", id).first();
+
+    return {
+      transaction,
+    };
+  });
+
+  app.get("/summary", async () => {
+    const summary = await knex("transactions")
+      .sum("amount", { as: "amount" })
+      .first();
+    return { summary };
+  });
+
   app.post("/", async (request, reply) => {
     const createdTransactionBodySchema = z.object({
       title: z.string().min(1).max(255),
@@ -24,4 +52,6 @@ export async function transactionsRoutes(app: FastifyInstance) {
 
     return reply.status(201).send("Record created successfully!");
   });
+
+  app.delete("/", async () => {});
 }
